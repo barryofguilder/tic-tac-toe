f28326956a50de5904bab1b023fadd3a12633a14
Jason Barry <barryofguilder@gmail.com>
2019-08-09 12:48:21 -0400

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,15 @@ module.exports = {
   parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'ember'
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended'
-  ],
+  plugins: ['ember', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:ember/recommended', 'plugin:prettier/recommended'],
   env: {
-    browser: true
+    browser: true,
   },
   rules: {
+    'prettier/prettier': 'error',
   },
   overrides: [
     // node files
@@ -28,25 +24,20 @@ module.exports = {
         'testem.js',
         'blueprints/*/index.js',
         'config/**/*.js',
-        'lib/*/index.js'
+        'lib/*/index.js',
       ],
-      excludedFiles: [
-        'app/**',
-      ],
+      excludedFiles: ['app/**'],
       parserOptions: {
         sourceType: 'script',
-        ecmaVersion: 2015
+        ecmaVersion: 2015,
       },
       env: {
         browser: false,
-        node: true
+        node: true,
       },
       plugins: ['node'],
-      extends: [
-        'plugin:node/recommended'
-      ],
-      rules: {
-      }
-    }
-  ]
+      extends: ['plugin:node/recommended'],
+      rules: {},
+    },
+  ],
 };
