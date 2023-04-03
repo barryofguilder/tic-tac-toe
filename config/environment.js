59d063349002f3216887830d5f56169b857be845
Jason Barry <jbarry@hey.com>
2023-04-02 20:58:02 -0400

    Update root url for render

--- config/environment.js
+++ config/environment.js
@@ -42,7 +42,7 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
-    ENV.rootURL = 'ttt';
+    // here you can enable a production-specific feature
   }
 
   return ENV;
