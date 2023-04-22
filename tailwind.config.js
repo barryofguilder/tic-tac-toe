3e6af9b3202c543e20bb271e203fbdf8ecf1c473
Jason Barry <jbarry@hey.com>
2023-04-22 14:13:35 -0400

    Use ring class instead of custom shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 module.exports = {
   content: ['./app/**/*.{hbs,js}', './app/index.html'],
   theme: {
-    extend: {
-      boxShadow: (theme) => ({
-        'outline-color': `0 0 0 3px ${theme('colors.teal.400')}`,
-      }),
-    },
+    extend: {},
   },
   plugins: [],
 };
