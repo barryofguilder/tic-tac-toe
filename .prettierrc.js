d3d51c68ec2cf5fd97010716bfb5ebfc0dcf8694
Jason Barry <jbarry@hey.com>
2023-08-05 13:12:29 -0400

    Install ember-template-imports

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,11 @@
 'use strict';
 
 module.exports = {
+  plugins: ['prettier-plugin-ember-template-tag'],
   printWidth: 100,
   overrides: [
     {
-      files: '*.{js,ts}',
+      files: '*.{js,ts,gjs,gts}',
       options: {
         singleQuote: true,
       },
